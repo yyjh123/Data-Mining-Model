b8f354eb0a7d61f34b77aab752b2b351778fed8e
yyjh123 <94735925+yyjh123@users.noreply.github.com>
2023-04-04 00:04:35 +0800

    update

--- 电力窃漏电用户自动识别/code/cm_plot.py
+++ 电力窃漏电用户自动识别/code/cm_plot.py
@@ -1,19 +1,18 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from sklearn.metrics import confusion_matrix
+
 
 def cm_plot(y, yp):
-  
-  from sklearn.metrics import confusion_matrix 
+    cm = confusion_matrix(y, yp)
 
-  cm = confusion_matrix(y, yp) 
-  
-  import matplotlib.pyplot as plt 
-  plt.matshow(cm, cmap=plt.cm.Greens) 
-  plt.colorbar() 
-  
-  for x in range(len(cm)): 
-    for y in range(len(cm)):
-      plt.annotate(cm[x,y], xy=(x, y), horizontalalignment='center', verticalalignment='center')
-  
-  plt.ylabel('True label') 
-  plt.xlabel('Predicted label') 
-  return plt
+    import matplotlib.pyplot as plt
+    plt.matshow(cm, cmap=plt.cm.Greens)
+    plt.colorbar()
+
+    for x in range(len(cm)):
+        for y in range(len(cm)):
+            plt.annotate(cm[x, y], xy=(x, y), horizontalalignment='center', verticalalignment='center')
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return plt
